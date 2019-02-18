e6a4b920f5bdd01de8b8ea21651e38e2659c16ae
chandan <chandan77.777@gmail.com>
2019-02-19 04:07:02 +0530

    1st commit

---
+++ staticDetails/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const sls = require('serverless-http')
+
+const app = express()
+app.get("/root", async (req,res,next)=>{
+   res.status(200).send('Hello World!')
+});
+
+
+module.exports.server= sls(app)
